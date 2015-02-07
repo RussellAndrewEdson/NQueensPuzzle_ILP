d83f981e7c99232400577a0ae3dd0a3e3ceaa2b1
RussellAndrewEdson <russell.andrew.edson@gmail.com>
2015-02-07 15:49:01 +1030

    Added libraries to project + GPL3 license at top of source file.

--- src/NQueensSolver.java
+++ src/NQueensSolver.java
@@ -1,3 +1,21 @@
+/*
+ * Formulates the N-Queens Puzzle as a 0-1 Integer Linear Program and solves it
+ * using the SCPSolver + GLPK tools.
+ * Copyright (C) 2015 Russell Andrew Edson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import scpsolver.constraints.LinearEqualsConstraint;
 import scpsolver.constraints.LinearSmallerThanEqualsConstraint;
 import scpsolver.lpsolver.LinearProgramSolver;
